a7e0164442a280ae18e7968729eeb66550273de2
GloriaPG <ing.gloriapalmagonzalez@gmail.com>
2017-02-24 11:33:25 -0600

    Plugin test testing :B

--- sentinella/test/test/test.py
+++ sentinella/test/test/test.py
@@ -14,35 +14,18 @@ hostname = os.uname()[1].split('.')[0]
 def get_stats(agent):
     yield From(agent.run_event.wait())
     config = agent.config['test']
-    plugin_key = config['plugin_key']
     logger.info('starting "get_stats" task for "%s"', hostname)
 
     while agent.run_event.is_set():
         yield From(asyncio.sleep(frequency))
         try:
-            data = { "{}".format(plugin_key):{}}
-
-            data["{}".format(plugin_key)] = {
-                                                "size_data":{
-                                                    "value":100,
-                                                    "type":"integer"
-                                                },
-                                                "mysql_mem":{
-                                                    "value":2.5,
-                                                    "type":"float"
-                                                },
-                                                "mysql_performance":{
-                                                    "value":50,
-                                                    "type":"percent"
-                                                }
-                                            }
-                                            
+            data = {'server_name': hostname,
+                    'measurements': []}
             logger.debug('connecting to data source')
             
             # [START] To be completed with plugin code
             # Here goes your logic
-            '''
-            Example:
+        
             
             instance = ''
             value = ''
@@ -58,10 +41,8 @@ def get_stats(agent):
                                          'tags': {'instance': instance},
                                          'value': value})
 
-            '''
-            # [END] To be completed with plugin code
-            
-            logger.debug('{}: {}%'.format(hostname,data))
+
+            logger.debug('{}: myplugin={}%'.format(hostname, data))
             yield From(agent.async_push(data))
         except:
             logger.exception('cannot get data source information')
